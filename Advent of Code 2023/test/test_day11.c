34919e4992a3e9bdd883f06ea46b6f468041b9b1
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-02 03:05:35 +1300

    2025 setup

--- Advent of Code 2023/test/test_day11.c
+++ Advent of Code 2023/test/test_day11.c
@@ -7,7 +7,8 @@
 
 void test_get_sum(){
     long long sum = 0;
-    int result = get_sum("test/day11.txt", &sum);
+    // int result = get_sum("test/day11.txt", &sum);
+    int result = get_sum("src/input11.txt", &sum);
     TEST_ASSERT_EQUAL_INT(0, result);
     TEST_ASSERT_EQUAL(374, sum);
 }
