045e82eb13a676baa6150ccd7d848b207641d7bc
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-05 00:58:18 +1300

    set up day4 part2

--- Advent of Code 2025/test/test_day4.c
+++ Advent of Code 2025/test/test_day4.c
@@ -13,3 +13,14 @@ void test_count_total(){
     TEST_ASSERT_EQUAL(1, result);
     TEST_ASSERT_EQUAL(0, total);
 }
+
+void test_count_total_v2(){
+    int total;
+    int result = count_total_v2("test/day4.txt", &total);
+    // int result = count_total_v2("src/input4.txt", &total);
+    TEST_ASSERT_EQUAL(0, result);
+    TEST_ASSERT_EQUAL(43, total);
+    result = count_total("fake.txt", &total);
+    TEST_ASSERT_EQUAL(1, result);
+    TEST_ASSERT_EQUAL(0, total);
+}
