1822740afb3c56527089c233caeb4429f7e496f0
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-05 01:06:02 +1300

    day4 part2

--- Advent of Code 2025/src/day4.c
+++ Advent of Code 2025/src/day4.c
@@ -49,19 +49,25 @@ int count_total_v2(const char* filename, int* total){
         return 1;
     }
     char** lines = grid.cells;
-    for (int r = 0; r < grid.rows; r++) {
-        char *line = lines[r];
-        for (int c = 0; c < grid.cols; c++) {
-            char ch = line[c];
-            if (ch == '@') {
-                Position pos = create_position(r, c, &grid);
-                if (!pos.is_valid) {
-                    cleanUp(&grid);
-                    return 1;
-                }
-                int count = count_target(&grid, &pos, '@');
-                if (count < 4) {
-                    (*total)++;
+    int found = 1;
+    while (found) {
+        found = 0;
+        for (int r = 0; r < grid.rows; r++) {
+            char *line = lines[r];
+            for (int c = 0; c < grid.cols; c++) {
+                char ch = line[c];
+                if (ch == '@') {
+                    Position pos = create_position(r, c, &grid);
+                    if (!pos.is_valid) {
+                        cleanUp(&grid);
+                        return 1;
+                    }
+                    int count = count_target(&grid, &pos, '@');
+                    if (count < 4) {
+                        (*total)++;
+                        found++;
+                        lines[r][c] = '.';
+                    }
                 }
             }
         }
--- Advent of Code 2025/test/test_day4.c
+++ Advent of Code 2025/test/test_day4.c
@@ -5,8 +5,8 @@
 
 void test_count_total(){
     int total;
-    // int result = count_total("test/day4.txt", &total);
-    int result = count_total("src/input4.txt", &total);
+    int result = count_total("test/day4.txt", &total);
+    // int result = count_total("src/input4.txt", &total);
     TEST_ASSERT_EQUAL(0, result);
     TEST_ASSERT_EQUAL(13, total);
     result = count_total("fake.txt", &total);
@@ -16,8 +16,8 @@ void test_count_total(){
 
 void test_count_total_v2(){
     int total;
-    int result = count_total_v2("test/day4.txt", &total);
-    // int result = count_total_v2("src/input4.txt", &total);
+    // int result = count_total_v2("test/day4.txt", &total);
+    int result = count_total_v2("src/input4.txt", &total);
     TEST_ASSERT_EQUAL(0, result);
     TEST_ASSERT_EQUAL(43, total);
     result = count_total("fake.txt", &total);
