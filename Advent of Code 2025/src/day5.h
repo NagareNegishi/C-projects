14b19722780fd3db3c45a9b1d2ea6770fcdb4e82
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-06 01:33:03 +1300

    part2 header

--- Advent of Code 2025/src/day5.h
+++ Advent of Code 2025/src/day5.h
@@ -5,6 +5,7 @@
 typedef struct {
     long long start;
     long long end;
+    int is_valid; // for part 2
 } Range;
 
 bool in_range(long long value, Range* range);
@@ -12,5 +13,12 @@ bool in_range(long long value, Range* range);
 int count_total(const char* filename, int* total);
 
 Range parse_range(const char* line);
+
+//part2
+int count_total_range(const char* filename, int* total);
+
+bool is_overlapping(Range* r1, Range* r2);
+
+Range merge_ranges(Range* r1, Range* r2);
 
 #endif // DAY_5_H
