85758074fc85ecc2ae0390274364a6192050c248
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-06 20:35:34 +1300

    update test

--- Advent of Code 2025/test/test_day6.c
+++ Advent of Code 2025/test/test_day6.c
@@ -53,3 +53,35 @@ void test_get_total(){
     TEST_ASSERT_EQUAL(1, result);
     TEST_ASSERT_EQUAL(0, total);
 }
+
+
+// part2
+TEST_CASE(0, 3, '*', 8544)
+TEST_CASE(1, 3, '+', 625)
+TEST_CASE(2, 3, '*', 3253600)
+TEST_CASE(3, 3, '+', 1058)
+void test_evaluate_problem_v2(int index, const int size, const char operation ,long long expected) {
+    const long long test_numbers[][5] = {
+        {64, 23 ,314},
+        {51, 387, 215},
+        {328, 64, 98},
+        {123, 45, 6}
+    };
+    Problem problem = {0};
+    memcpy(problem.numbers, test_numbers[index], size * sizeof(long long));
+    problem.size = size;
+    problem.operation = operation;
+    long long result = evaluate_problem_v2(&problem);
+    TEST_ASSERT_EQUAL(expected, result);
+}
+
+void test_get_total_v2(){
+    long long total = 0;
+    // int result = get_total_v2("test/day6.txt", &total);
+    int result = get_total_v2("src/input6.txt", &total);
+    TEST_ASSERT_EQUAL(0, result);
+    TEST_ASSERT_EQUAL(4277556, total);
+    result = get_total_v2("fake.txt", &total);
+    TEST_ASSERT_EQUAL(1, result);
+    TEST_ASSERT_EQUAL(0, total);
+}
