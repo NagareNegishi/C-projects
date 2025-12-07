f515c2465fe987be3cfd940cf770d302699196da
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-07 18:34:52 +1300

    day7 header

---
+++ Advent of Code 2025/src/day7.h
@@ -0,0 +1,19 @@
+#ifndef DAY7_H
+#define DAY7_H
+
+typedef struct {
+    int indexes[150];
+    int size;
+} Beams;
+
+// Part 1
+// we know the case the bean split on the edge of the grid never happens in the input
+// and no even number of rows never contains meaningful symbols -> can skip those rows
+
+/**
+ * Parse a line of beam input and return the split beams
+ */
+Beams split_beams(const char* line, Beams* previous_beams);
+
+int get_total(const char* filename, int* total);
+#endif // DAY7_H
