0fee7c40664c55b88f78804ffaeedb2d4703dd41
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-08 14:47:20 +1300

    redefine day7 part2

--- Advent of Code 2025/src/day7.h
+++ Advent of Code 2025/src/day7.h
@@ -19,17 +19,10 @@ int get_total(const char* filename, int* total);
 
 
 //part 2
-// I need to make tree structure to represent the beam splits
-// and make recursive function to traverse the tree and count the possible paths
-
-typedef struct BeamNode {
-    struct BeamNode* left;
-    struct BeamNode* right;
-    int left_index;
-    int right_index;
-} BeamNode;
-
-
+// since I already have beam structure, I can reuse it here
+// create a list of Beams
+// each row, each beam position, keep track of number of ways to reach there
 
 int get_total_v2(const char* filename, long long* total);
+
 #endif // DAY7_H
