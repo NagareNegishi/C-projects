5b167e71439e0c2ffaa801c1183903ee32a64134
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-09 22:10:27 +1300

    readPointsFromFile

--- Advent of Code 2025/src/day9.c
+++ Advent of Code 2025/src/day9.c
@@ -2,8 +2,33 @@
 #include "day9.h"
 
 
-int getMaxArea(const char* filename, long long* area);
+int getMaxArea(const char* filename, long long* area){
+    return 0;
+}
 
-int readPointsFromFile(const char* filename, Point* points, int* point_size);
+int readPointsFromFile(const char* filename, Point* points, int* point_size){
+    *point_size = 0;
+    FILE* in = fopen(filename, "r");
+    if (in == NULL) {
+        return 1;
+    }
+    char buffer[20];
+    while (fgets(buffer, sizeof(buffer), in) != NULL) {
+        int x, y;
+        if (sscanf(buffer, "%d,%d", &x, &y) == 2) {
+            points[*point_size].x = x;
+            points[*point_size].y = y;
+            (*point_size)++;
+        } else {
+            fclose(in);
+            return 1;
+        }
+    }
+    fclose(in);
+    return 0;
+}
 
-BestCorners findBestCorners(Point* points, int point_size);
+BestCorners findBestCorners(Point* points, int point_size){
+    BestCorners corners = {0};
+    return corners;
+}
