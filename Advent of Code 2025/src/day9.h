9bf0a037aa19befeef0e057c3ae9445c3b8185a6
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-09 21:52:45 +1300

    day9 header

---
+++ Advent of Code 2025/src/day9.h
@@ -0,0 +1,34 @@
+#ifndef DAY9_H
+#define DAY9_H
+
+/**
+ * largest area can be made from:
+ * - best top left corner and bottom right corner
+ * - or best top right corner and bottom left corner
+ *
+ * File input is random, so first I need to read all point and sort them by x and y
+ */
+
+#define MAX_POINTS 500
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+
+typedef struct {
+    Point top_left;
+    Point top_right;
+    Point bottom_left;
+    Point bottom_right;
+} BestCorners;
+
+int getMaxArea(const char* filename, long long* area);
+
+int readPointsFromFile(const char* filename, Point* points, int* point_size);
+
+BestCorners findBestCorners(Point* points, int point_size);
+
+
+#endif // DAY9_H
