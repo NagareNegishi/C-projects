16c5fdafb0d95c59bd73a67e8a8c58ce77c44f5b
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-11 00:56:41 +1300

    setup day10

---
+++ Advent of Code 2025/src/day10.h
@@ -0,0 +1,24 @@
+#ifndef DAY10_H
+#define DAY10_H
+#include <stdint.h>
+
+/**
+ * [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+ * for part 1, completely ignore joltage requirements {}
+ * both light diagram [.##.]
+ * and buttons (3) (1,3) (2) (2,3) (0,2) (0,1)
+ * can be represented as binary numbers, where # is 1 and . is 0
+ */
+
+int get_fewest_button_presses(const char* filename, long long* total);
+
+/**
+ * Check each line and return fewest button presses needed
+ * return -1 on error
+ */
+int check_machine(const char* line);
+
+uint16_t convert_diagram(const char* diagram);
+uint16_t convert_buttons(const char* buttons);
+
+#endif // DAY10_H
