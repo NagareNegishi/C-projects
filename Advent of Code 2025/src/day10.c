d0fdc88924d335db2226a0865062f623576b51b6
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-11 01:08:30 +1300

    get_fewest_button_presses

--- Advent of Code 2025/src/day10.c
+++ Advent of Code 2025/src/day10.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include "day10.h"
 
-int get_fewest_button_presses(const char* filename, long long* total);
+int get_fewest_button_presses(const char* filename, long long* total){
+    *total = 0;
+    FILE* in = fopen(filename, "r");
+    if (in == NULL) return -1;
+
+    char line[256];
+    while (fgets(line, sizeof(line), in) != NULL) {
+        int presses = check_machine(line);
+        if (presses == -1) {
+            fclose(in);
+            return -1;
+        }
+        *total += presses;
+    }
+    fclose(in);
+    return 0;
+}
 
 /**
  * Check each line and return fewest button presses needed
@@ -11,3 +27,5 @@ int check_machine(const char* line);
 
 uint16_t convert_diagram(const char* diagram);
 uint16_t convert_buttons(const char* buttons);
+
+
