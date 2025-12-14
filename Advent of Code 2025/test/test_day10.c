ad141d245ab9561647e4798e434d1e8fd3bcf11d
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-14 22:37:13 +1300

    update test

--- Advent of Code 2025/test/test_day10.c
+++ Advent of Code 2025/test/test_day10.c
@@ -9,39 +9,30 @@ void test_get_fewest_button_presses(){
     int result = get_fewest_button_presses("test/day10.txt", &total);
     // int result = get_fewest_button_presses("src/input10.txt", &total);
     TEST_ASSERT_EQUAL(0, result);
-    TEST_ASSERT_EQUAL(7, total); // Replace with the expected total
+    TEST_ASSERT_EQUAL(7, total);
     result = get_fewest_button_presses("fake.txt", &total);
     TEST_ASSERT_EQUAL(1, result);
     TEST_ASSERT_EQUAL(0, total);
 }
 
-// TEST_CASE("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n", 2)
-// TEST_CASE("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n", 3)
-// TEST_CASE("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}\n", 7)
-void test_check_machine(){//const char* line, const int expected){
-    // TEST_ASSERT_EQUAL(3, check_machine(line));
-    // TEST_ASSERT_EQUAL(-1, check_machine("invalid input\n"));
+
+void test_check_machine(){
     TEST_ASSERT_EQUAL(2, check_machine("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n"));
     TEST_ASSERT_EQUAL(3, check_machine("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n"));
     TEST_ASSERT_EQUAL(2, check_machine("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}\n"));
     TEST_ASSERT_EQUAL(-1, check_machine("invalid input\n"));
 }
 
-// TEST_CASE("[.##.]", 0b0110)
-// TEST_CASE("[...#.]", 0b00010)
-// TEST_CASE("[.###.#]", 0b011101)
-void test_convert_diagram(){//(char* diagram, const uint16_t expected){
-    // TEST_ASSERT_EQUAL(expected, convert_diagram(diagram));
+
+void test_convert_diagram(){
     TEST_ASSERT_EQUAL(0b0110, convert_diagram("[.##.]"));
     TEST_ASSERT_EQUAL(0b00010, convert_diagram("[...#.]"));
     TEST_ASSERT_EQUAL(0b011101, convert_diagram("[.###.#]"));
 }
 
-// TEST_CASE("(3)", 0b1000)
-// TEST_CASE("(0,2,3,4)", 0b11101)
-void test_convert_buttons(){//char* buttons, const uint16_t expected){
-    // TEST_ASSERT_EQUAL(expected, convert_buttons(buttons));
-    TEST_ASSERT_EQUAL(0b1000, convert_buttons("(3)"));
-    TEST_ASSERT_EQUAL(0b11101, convert_buttons("(0,2,3,4)"));
+
+void test_convert_buttons(){//
+    TEST_ASSERT_EQUAL(0b0001, convert_buttons("(3)", 4));
+    TEST_ASSERT_EQUAL(0b10111, convert_buttons("(0,2,3,4)", 5));
 }
 
