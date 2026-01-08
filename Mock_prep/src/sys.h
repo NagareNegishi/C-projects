8d8d51b4c652c1c700c9ed159553d71877352a6f
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-08 22:40:39 +1300

    define SYS_H

---
+++ Mock_prep/src/sys.h
@@ -0,0 +1,9 @@
+#ifndef SYS_H
+#define SYS_H
+
+/**
+ * Function to wrap system calls.
+ * use ifreq structure and socket to get some information about network interfaces.
+ * use some logging mechanism to log errors.
+ */
+#endif
