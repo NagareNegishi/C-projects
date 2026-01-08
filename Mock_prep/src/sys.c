5421267248637461085ece3b7b82c814374821f7
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-08 23:05:34 +1300

    basic impl

--- Mock_prep/src/sys.c
+++ Mock_prep/src/sys.c
@@ -1,9 +1,13 @@
 #include "sys.h"
 #include <stdio.h>
+
+#include <string.h>
+#include <unistd.h>        // for close
 #include <sys/ioctl.h>    // General ioctl stuff
 #include <net/if.h>        // Network interface commands (SIOCGIFADDR, etc.)
 #include <linux/fs.h>      // Filesystem commands
 #include <termios.h>       // Terminal commands
+// add some logging later
 
 int sys_io() {
     // Placeholder implementation
@@ -16,6 +20,17 @@ int sys_io() {
         perror("Socket creation failed");
         return -1;
     }
+    // Example ioctl call (not functional, just illustrative)
+    strcpy(ifr.ifr_name, "eth0");
+    if (ioctl(sock, SIOCGIFADDR, &ifr) < 0) {
+        perror("ioctl SIOCGIFADDR failed");
+        close(sock);
+        return -1;
+    }
+
+    // if we reach here, everything is fine, print for now
+    printf("ioctl SIOCGIFADDR succeeded.\n");
+    close(sock);
 
 
     return 0;
