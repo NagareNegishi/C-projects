817b4527182d06bd5026101d6af2dd5b6be101e1
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-08 22:56:49 +1300

    prepare socket and struct

--- Mock_prep/src/sys.c
+++ Mock_prep/src/sys.c
@@ -1,8 +1,22 @@
 #include "sys.h"
 #include <stdio.h>
+#include <sys/ioctl.h>    // General ioctl stuff
+#include <net/if.h>        // Network interface commands (SIOCGIFADDR, etc.)
+#include <linux/fs.h>      // Filesystem commands
+#include <termios.h>       // Terminal commands
 
 int sys_io() {
     // Placeholder implementation
     printf("sys_io function called.\n");
+
+    // prepare to use ifreq structure and socket
+    struct ifreq ifr;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("Socket creation failed");
+        return -1;
+    }
+
+
     return 0;
 }
--- Mock_prep/test/test_sys.c
+++ Mock_prep/test/test_sys.c
@@ -1,6 +1,11 @@
 #include "unity.h"
 #include "sys.h"
 
+#include <sys/ioctl.h>    // General ioctl stuff
+#include <net/if.h>        // Network interface commands (SIOCGIFADDR, etc.)
+#include <linux/fs.h>      // Filesystem commands
+#include <termios.h>       // Terminal commands
+
 void setUp(void) {
     // This is run before EACH TEST
 }
