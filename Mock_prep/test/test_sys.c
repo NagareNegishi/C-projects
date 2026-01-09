7e5d5f08da1059dd2ee340542dd14f560afb083f
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-09 16:42:51 +1300

    setup

--- Mock_prep/test/test_sys.c
+++ Mock_prep/test/test_sys.c
@@ -1,10 +1,26 @@
 #include "unity.h"
 #include "sys.h"
 
-#include <sys/ioctl.h>    // General ioctl stuff
+#include <string.h>
+#include <unistd.h>        // for close
+#include <sys/ioctl.h>    // General ioctl stuff https://linux.die.net/man/7/netdevice
 #include <net/if.h>        // Network interface commands (SIOCGIFADDR, etc.)
 #include <linux/fs.h>      // Filesystem commands
 #include <termios.h>       // Terminal commands
+
+// for in_addr printing https://linux.die.net/man/3/inet_ntoa
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// fff plugin is used
+// #include "fff.h"
+// DEFINE_FFF_GLOBALS;
+
+// Declare fakes for the functions we need to control
+FAKE_VALUE_FUNC(int, socket, int, int, int);
+// FAKE_VALUE_FUNC(int, ioctl, int, unsigned long, void*);
+
 
 void setUp(void) {
     // This is run before EACH TEST
