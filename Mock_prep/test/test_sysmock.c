62c63be7afcaf14569dcce6e276aea37e510afa3
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-10 19:34:14 +1300

    select mock

--- Mock_prep/test/test_sysmock.c
+++ Mock_prep/test/test_sysmock.c
@@ -12,5 +12,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-// CMock generated mock header
+// generated mock header in build/test/mocks
+// my current setup use fff, if I comment out fff plugin in ceedling.yml, it uses cmock instead
+// FFF is preferred here as I already patched it to support FAKE_VALUE_FUNC3_VARARG
 #include "mock_system_wrapper.h"
